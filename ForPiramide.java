b156e40fb28ec063384b01ca07875dda2faebe2e
carlam97 <carmartins04@hotmail.com>
2021-09-09 17:13:32 +0000

    For em formato de piramide

---
+++ ForPiramide.java
@@ -0,0 +1,11 @@
+public class ForPiramide{
+    public static void main(String args[]){
+        for(int n=1; n<=4; n++){
+            for(int i=1; i<=n; i++)
+            { 
+        System.out.print(n*i +" ");
+    }
+    System.out.println();
+        }
+    }
+}
